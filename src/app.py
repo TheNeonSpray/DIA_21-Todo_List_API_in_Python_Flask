2860f966fd53b35253b88bbe72a2bb3c8c1bcd61
Alex Spinoza <alexandraeb20@gmail.com>
2021-02-25 00:17:08 +0000

    Finalización de la práctica interactiva

---
+++ src/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request, json
+app = Flask(__name__)
+
+todos = [
+    { "label": "Hacer compras", "done": False },
+    { "label": "Lavar el auto", "done": False },
+    { "label": "Tender la ropa", "done": False }
+]
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    del todos [position]
+    return jsonify(todos)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
+
